42c5634b7d9577ea72dcb5b10eb5d473e7a0eeda
LightTab2 <azorro12@gmail.com>
2025-02-05 00:21:17 +0100

    NewProjectDialog fixes with error checking

--- src/Editor/NewProjectDialog.cpp
+++ src/Editor/NewProjectDialog.cpp
@@ -1,65 +1,180 @@
 #include "NewProjectDialog.h"
 
-#include <QFileDialog>
+#include <QPushButton>
 
-NewProjectDialog::NewProjectDialog(QWidget *parent)
-	: QDialog(parent)
+#include <boost/filesystem.hpp>
+
+// TODO: this is actually pretty good. I should release this as a standalone custom component one day. And make portability a boolean
+
+NewProjectDialog::NewProjectDialog(QWidget* parent) : QDialog(parent)
 {
-	//todo: load from QSettings
 	ui_.setupUi(this);
 
-	ui_.projectNameLineEdit->setText(projectName_);
-	ui_.projectPathLineEdit->setText(projectCatalog_.path() + '/' + projectName_);
+	// Loading options from QSettings, defaulting to initialized value
+	QSettings settings(QSettings::IniFormat, QSettings::UserScope, QCoreApplication::organizationName(), QCoreApplication::applicationName());
+	projectName_ = settings.value("Project Name", projectName_).toString();
+	projectPath_ = settings.value("Project Path", projectPath_.path()).toString();
+	ui_.projectName_lineEdit->setText(projectName_);
+	ui_.projectPath_lineEdit->setText(projectPath_.path() + '/' + projectName_);
 
-	connect(ui_.buttonBox, &QDialogButtonBox::accepted, this, &NewProjectDialog::accept);
-	connect(ui_.buttonBox, &QDialogButtonBox::rejected, this, &NewProjectDialog::reject);
-	//todo: change ui names
-	//todo: connections as variables, to connect and disconnect, to avoid infinite loop
-	connect(ui_.selectDirectoryToolButton, &QToolButton::clicked, this, &NewProjectDialog::projectCatalogSelection);
-	connect(ui_.projectNameLineEdit, &QLineEdit::textChanged, this, [&](const QString& name)
-	{
-		projectName_ = name;
-		//ui_.projectPathLineEdit->disconnect
-		ui_.projectPathLineEdit->setText(projectCatalog_.path() + '/' + projectName_);
-		//ui_.projectPathLineEdit->connect
+	// Choosing directory pop up will begin from the current directory
+	if (projectPath_.exists())
+		chooseDirDialog_.setDirectory(projectPath_);
+
+	// Buttons from QButtonBox decide whether a new project will be created or the process is cancelled
+	connect(ui_.dialogButtons_buttonBox, &QDialogButtonBox::accepted, this, &NewProjectDialog::accept);
+	connect(ui_.dialogButtons_buttonBox, &QDialogButtonBox::rejected, this, &NewProjectDialog::reject);
+
+	// Create directory choosing menu on "..." button click 
+	connect(ui_.chooseDirectory_toolButton, &QToolButton::clicked, &chooseDirDialog_, [&](bool checked) { chooseDirDialog_.open(); });
+	// Allow only to pick Directories
+	chooseDirDialog_.setFileMode(QFileDialog::Directory);
+	chooseDirDialog_.setViewMode(QFileDialog::Detail);
+
+	connect(ui_.projectName_lineEdit, &QLineEdit::textChanged, this, &NewProjectDialog::projectNameEdited);
+	connect(ui_.projectPath_lineEdit, &QLineEdit::textChanged, this, &NewProjectDialog::projectPathEdited);
+
+	// Picking a file from a popup from "..." button will change the projectPath
+	connect(&chooseDirDialog_, &QFileDialog::fileSelected, this, [&](const QString& selectedDirectory) 
+	{ 
+		ui_.projectPath_lineEdit->setText(QDir::cleanPath(QDir(selectedDirectory).absolutePath()) + '/' + projectName_); 
 	});
-	/*connect(ui_.projectPathLineEdit, &QLineEdit::textChanged, this, [&](const QString& path)
-	{
-		projectCatalog_ = QDir(path);
-		//todo: check if right is the wrong, unoptimal function. Replace all \ to match Linux subsystem path names
-		ui_.projectNameLineEdit->setText(path.right(path.size() - std::max(path.lastIndexOf('/'), path.lastIndexOf('\\'))));
-		// todo: add invalid path error!
-	});*/
+
+	/// Allows only for the correct (portable on many filesystem) project name characters to be entered
+	QRegularExpression allowedName("^(?!-)[a-zA-Z0-9-_]+(?:[.][a-zA-Z0-9-_]{1,3})*$");
+	correctNameValidator.reset(new QRegularExpressionValidator(allowedName, this));
+	ui_.projectName_lineEdit->setValidator(correctNameValidator.get());
+	
+	checkInputErrors();
 }
 
-NewProjectDialog::~NewProjectDialog()
-{}
-
-QDir NewProjectDialog::getProjectCatalog()
+QDir NewProjectDialog::projectPath()
 {
-	return projectCatalog_;
+	return projectPath_;
 }
 
-QString NewProjectDialog::getProjectName()
+QString NewProjectDialog::projectName()
 {
 	return projectName_;
 }
 
-void NewProjectDialog::projectCatalogSelection()
+void NewProjectDialog::projectNameEdited(const QString& newName)
 {
-	QFileDialog fileDialog = QFileDialog(nullptr, tr("Choose project location"), projectCatalog_.path());
-	fileDialog.setFileMode(QFileDialog::Directory);
-	fileDialog.setViewMode(QFileDialog::Detail);
+	projectName_ = newName;
 
-	// todo check if filename, project path is correct
-	if (fileDialog.exec())
-	{
-		projectCatalog_ = fileDialog.directory();
-		ui_.projectPathLineEdit->setText(projectCatalog_.path() + '/' + projectName_);
+	ui_.projectPath_lineEdit->disconnect();
+	ui_.projectPath_lineEdit->setText(projectPath_.path() + '/' + newName);
+	connect(ui_.projectPath_lineEdit, &QLineEdit::textChanged, this, &NewProjectDialog::projectPathEdited);
 
-	}
-	else 
+	checkInputErrors();
+}
+
+void NewProjectDialog::projectPathEdited(const QString& newPath)
+{
+	qsizetype lastSlash = newPath.lastIndexOf('\\');
+	if (lastSlash != -1)
 	{
-		// TODO error	
+		ui_.projectPath_lineEdit->setText(QDir::cleanPath(newPath));
+		return;
 	}
+
+	lastSlash = newPath.lastIndexOf('/');
+	if (lastSlash == -1)
+	{
+		raiseInputError("the directory cannot be empty", false, true);
+		return;
+	}
+
+	projectPath_ = newPath.left(lastSlash);
+	projectName_ = newPath.sliced(lastSlash + 1);
+
+	ui_.projectName_lineEdit->disconnect();
+	ui_.projectName_lineEdit->setText(projectName_);
+	connect(ui_.projectName_lineEdit, &QLineEdit::textChanged, this, &NewProjectDialog::projectNameEdited);
+
+	checkInputErrors();
+}
+
+bool NewProjectDialog::checkInputErrors()
+{
+	// Reset and assume everything is alright
+	removeInputError();
+
+	bool bError = false;
+
+	if (!boost::filesystem::portable_file_name(projectName_.toLocal8Bit().constData()))
+	{
+		raiseInputError("project name is invalid (hover over it, to see its tooltip)", true);
+		bError = true;
+	}
+
+	if (projectName_.trimmed().isEmpty())
+	{
+		raiseInputError("project name cannot be empty", true);
+		bError = true;
+	}
+
+	if (!QDir::isAbsolutePath(projectPath_.path()))
+	{
+		raiseInputError("project path must be an absolute path", false, true);
+		bError = true;
+	}
+
+	if (!bError && !projectPath_.exists())
+		raiseInputWarning("project path doesn't exist, it will be created if possible", false, true);
+
+	return bError;
+}
+
+void NewProjectDialog::raiseInputError(const QString& error, bool bNameError, bool bPathError)
+{
+	ui_.error_label->setText("Error: " + error + '!');
+	QPushButton* okButton = ui_.dialogButtons_buttonBox->button(QDialogButtonBox::StandardButton::Ok);
+	if (okButton)
+	{
+		okButton->setDisabled(true);
+		okButton->clearFocus(); // todo: doesn't work, find out why
+	}
+
+	if (bNameError)
+	{
+		ui_.projectName_label->setStyleSheet("QLabel { color: red; }");
+		ui_.projectName_lineEdit->setStyleSheet("QLineEdit { border: 2px solid red; }");
+	}
+
+	if (bPathError)
+	{
+		ui_.projectPath_label->setStyleSheet("QLabel { color: red; }");
+		ui_.projectPath_lineEdit->setStyleSheet("QLineEdit { border: 2px solid red; }");
+	}
+}
+
+void NewProjectDialog::raiseInputWarning(const QString& warning, bool bNameWarning, bool bPathWarning)
+{
+	ui_.error_label->setText("Warning: " + warning + '!');
+
+	if (bNameWarning)
+	{
+		ui_.projectName_label->setStyleSheet("QLabel { color: orange; }");
+		ui_.projectName_lineEdit->setStyleSheet("QLineEdit { border: 2px solid orange; }");
+	}
+
+	if (bPathWarning)
+	{
+		ui_.projectPath_label->setStyleSheet("QLabel { color: orange; }");
+		ui_.projectPath_lineEdit->setStyleSheet("QLineEdit { border: 2px solid orange; }");
+	}
+}
+
+void NewProjectDialog::removeInputError()
+{
+	ui_.error_label->setText("");
+	QPushButton* okButton = ui_.dialogButtons_buttonBox->button(QDialogButtonBox::StandardButton::Ok);
+	if (okButton)
+		okButton->setDisabled(false);
+
+	ui_.projectName_label->setStyleSheet("QLabel { }");
+	ui_.projectName_lineEdit->setStyleSheet("QLineEdit {}");
+	ui_.projectPath_label->setStyleSheet("QLabel { }");
+	ui_.projectPath_lineEdit->setStyleSheet("QLineEdit { }");
 }
--- src/Editor/NewProjectDialog.h
+++ src/Editor/NewProjectDialog.h
@@ -1,27 +1,57 @@
 #pragma once
-
 #include <QDialog>
+
 #include <QDir>
+#include <QFileDialog>
+#include <QRegularExpressionValidator>
+#include <QSettings>
 
 #include "ui_NewProjectDialog.h"
 
+/// Pop-up dialog, in which User can create a new Project
 class NewProjectDialog : public QDialog
 {
 	Q_OBJECT
 
 public:
 	NewProjectDialog(QWidget *parent = nullptr);
-	~NewProjectDialog();
 
-	QDir getProjectCatalog();
-	QString getProjectName();
+	NewProjectDialog(const NewProjectDialog&)            noexcept = delete;
+	NewProjectDialog(NewProjectDialog&&)                 noexcept = default;
+	NewProjectDialog& operator=(const NewProjectDialog&) noexcept = delete;
+
+	QDir    projectPath();
+	QString projectName();
 
 private:
-	void projectCatalogSelection();
+	void projectNameEdited(const QString& name);
+	void projectPathEdited(const QString& name);
 
-	QDir projectCatalog_ = QDir::currentPath();
-	QString projectName_ = "New Project";
+	bool checkInputErrors();
 
+	/// The dialog enters "error" state, displaying the error and highlighting, which Input caused the error
+	/// \param error The error message displayed as orange text
+	/// \param bNameError Name's input box and label will become red, if true
+	/// \param bPathError Path's input box and label will become red, if true
+	void raiseInputError(const QString& error, bool bNameError = false, bool bPathError = false);
 
-	Ui::NewProjectDialogClass ui_;
+	/// The dialog enters "warning" state, displaying the warning and highlighting, which Input caused the error
+	/// \param warning The warning message displayed as orange text
+	/// \param bNameWarning Name's input box and label will become orange, if true
+	/// \param bPathWarning Path's input box and label will become orange, if true
+	void raiseInputWarning(const QString& warning, bool bNameWarning = false, bool bPathWarning = false);
+
+	/// Also removes warnings
+	void removeInputError();
+
+	QDir    projectPath_ = QDir::currentPath();
+	QString projectName_ = "NewProject";
+
+	/// Invoked when clicking button with "..."
+	QFileDialog chooseDirDialog_ = QFileDialog(nullptr, tr("Choose project location"), projectPath_.path());
+
+	/// Allows only for the correct (portable on many filesystem) project name characters to be entered
+	std::unique_ptr<QRegularExpressionValidator> correctNameValidator;
+
+	Ui::NewProjectDialog ui_;
 };
